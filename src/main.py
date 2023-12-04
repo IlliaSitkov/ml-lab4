9927b304a199652684d0da8bbd630463d8b8214d
Illia Sitkov <illiasitkov@gmail.com>
2023-12-04 22:36:34 +0200

    split main into functions

--- src/main.py
+++ src/main.py
@@ -15,36 +15,6 @@ from sklearn.multioutput import MultiOutputClassifier
 from joblib import dump
 
 
-nltk.download("wordnet", "/kaggle/output")
-
-wordnet_zip_path = "/kaggle/output/corpora/wordnet.zip"
-
-wordnet_extract_path = "/kaggle/output/corpora/"
-
-nltk.data.path.append("/kaggle/output")
-
-if not os.path.exists(
-        os.path.join(wordnet_extract_path, "corpora", "wordnet")
-):
-    with zipfile.ZipFile(wordnet_zip_path, "r") as zip_ref:
-        zip_ref.extractall(wordnet_extract_path)
-
-
-train_dataset = pd.read_csv("/kaggle/input/kma-ml2/kmaml223/train.csv")
-train_comments = train_dataset["comment_text"].values
-print("Train dataset number of samples: " + str(train_comments.shape[0]))
-
-
-test_dataset = pd.read_csv("/kaggle/input/kma-ml2/kmaml223/test.csv")
-test_comments = test_dataset["comment_text"].values
-print("Test dataset number of samples: " + str(test_comments.shape[0]))
-
-dataset_size = 100000
-train_dataset = train_dataset.sample(n=dataset_size, random_state=42)
-train_comments = train_dataset["comment_text"].values
-train_labels = train_dataset.iloc[:, 2:].to_numpy()
-
-
 def lemmatize_text(text):
     lemmatizer = WordNetLemmatizer()
     words = word_tokenize(text)
@@ -64,73 +34,91 @@ def preprocess_comments(comments):
     return comments_lem
 
 
-train_comments_preproc = preprocess_comments(train_comments)
-test_comments_preproc = preprocess_comments(test_comments)
+def train_model():
+    nltk.download("wordnet", "/kaggle/output")
+    wordnet_zip_path = "/kaggle/output/corpora/wordnet.zip"
+    wordnet_extract_path = "/kaggle/output/corpora/"
+    nltk.data.path.append("/kaggle/output")
 
+    if not os.path.exists(
+            os.path.join(wordnet_extract_path, "corpora", "wordnet")
+    ):
+        with zipfile.ZipFile(wordnet_zip_path, "r") as zip_ref:
+            zip_ref.extractall(wordnet_extract_path)
 
-tfidf_vectorizer = TfidfVectorizer(
-    max_features=1024, strip_accents="unicode", stop_words="english"
-)
+    train_dataset = pd.read_csv("/kaggle/input/kma-ml2/kmaml223/train.csv")
+    train_comments = train_dataset["comment_text"].values
+    print("Train dataset number of samples: " + str(train_comments.shape[0]))
 
-train_feature_vectors_tfidf = tfidf_vectorizer.fit_transform(
-    train_comments_preproc
-).toarray()
-print(len(train_feature_vectors_tfidf))
+    test_dataset = pd.read_csv("/kaggle/input/kma-ml2/kmaml223/test.csv")
+    test_comments = test_dataset["comment_text"].values
+    print("Test dataset number of samples: " + str(test_comments.shape[0]))
 
-test_feature_vectors_tfidf = tfidf_vectorizer\
-    .transform(test_comments_preproc).toarray()
+    dataset_size = 100000
+    train_dataset = train_dataset.sample(n=dataset_size, random_state=42)
+    train_comments = train_dataset["comment_text"].values
+    train_labels = train_dataset.iloc[:, 2:].to_numpy()
 
+    train_comments_preproc = preprocess_comments(train_comments)
+    test_comments_preproc = preprocess_comments(test_comments)
 
-pca4 = PCA(n_components=0.95)
-reduced_train4_pca = pca4.fit_transform(train_feature_vectors_tfidf)
-print(reduced_train4_pca.shape)
+    tfidf_vectorizer = TfidfVectorizer(
+        max_features=1024, strip_accents="unicode", stop_words="english"
+    )
 
-reduced_test4_pca = pca4.transform(test_feature_vectors_tfidf)
-print(reduced_test4_pca.shape)
+    train_feature_vectors_tfidf = tfidf_vectorizer.fit_transform(
+        train_comments_preproc
+    ).toarray()
+    print(len(train_feature_vectors_tfidf))
 
+    test_feature_vectors_tfidf = tfidf_vectorizer \
+        .transform(test_comments_preproc).toarray()
 
-X = reduced_train4_pca
-y = train_labels
+    pca4 = PCA(n_components=0.95)
+    reduced_train4_pca = pca4.fit_transform(train_feature_vectors_tfidf)
+    print(reduced_train4_pca.shape)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+    reduced_test4_pca = pca4.transform(test_feature_vectors_tfidf)
+    print(reduced_test4_pca.shape)
 
-classifier = MultiOutputClassifier(LogisticRegression())
+    X = reduced_train4_pca
+    y = train_labels
 
-classifier.fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
-y_pred = classifier.predict(X_test)
+    classifier = MultiOutputClassifier(LogisticRegression())
 
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")
+    classifier.fit(X_train, y_train)
 
+    y_pred = classifier.predict(X_test)
 
-test_pred = classifier.predict(reduced_test4_pca)
-test_pred
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy: {accuracy}")
 
+    test_pred = classifier.predict(reduced_test4_pca)
 
-columns = [
-    "id",
-    "toxic",
-    "severe_toxic",
-    "obscene",
-    "threat",
-    "insult",
-    "identity_hate",
-]
+    columns = [
+        "id",
+        "toxic",
+        "severe_toxic",
+        "obscene",
+        "threat",
+        "insult",
+        "identity_hate",
+    ]
 
-ids = test_dataset["id"].values
+    ids = test_dataset["id"].values
 
+    result = np.column_stack((ids, *test_pred.T)).tolist()
 
-result = np.column_stack((ids, *test_pred.T)).tolist()
+    df = pd.DataFrame(result, columns=columns)
 
-df = pd.DataFrame(result, columns=columns)
+    df.to_csv("sample_submission.csv", index=False)
 
-df.to_csv("sample_submission.csv", index=False)
+    print(df)
 
-print(df)
-
-dump(classifier, "my_model.joblib")
-dump(tfidf_vectorizer, "vectorizer.joblib")
-dump(pca4, 'pca_model.joblib')
+    dump(classifier, "my_model.joblib")
+    dump(tfidf_vectorizer, "vectorizer.joblib")
+    dump(pca4, 'pca_model.joblib')
