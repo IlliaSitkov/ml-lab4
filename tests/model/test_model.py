c5d69d2362223c7561152e3201773854fc62086c
Illia Sitkov <illiasitkov@gmail.com>
2023-12-04 23:35:11 +0200

    add tests

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -2,6 +2,26 @@ import pytest
 from src.main import preprocess_comments
 
 
+def test_label_length(predictor, vectorizer, dim_reducer):
+    """Predictor returns label with 6 classes"""
+    label_a = get_label(text="Stupid peace of shit stop deleting my stuff asshole go die and fall in a hole go to hell!", predictor=predictor, vectorizer=vectorizer, dim_reducer=dim_reducer)
+    assert len(label_a) == 6
+
+
+def test_vectorizer_numerical_1024(vectorizer):
+    """Vectorizer converts text to array of numbers of length 1024"""
+    vect = vectorizer.transform(["Stupid peace of shit stop deleting my stuff asshole go die and fall in a hole go to hell!"])
+    vect = vect.toarray()
+    assert len(vect) == 1024 and all(isinstance(element, (int, float)) for element in vect)
+
+
+def test_reducer_reduces(vectorizer, dim_reducer):
+    """Dimensionality reducer reduces the dimentionality of the vector"""
+    vect = vectorizer.transform(["Stupid peace of shit stop deleting my stuff asshole go die and fall in a hole go to hell!"])
+    vect_reduced = dim_reducer.transform(vect.toarray())
+    assert len(vect) >= len(vect_reduced)
+
+
 @pytest.mark.parametrize("inp_a, inp_b, label", [
     ("Stupid peace of shit stop deleting my stuff asshole go die and fall in a hole go to hell!",
      "Stupid peace of shit stop deleting my stuff asshole go die", [1, 0, 1, 0, 1, 0])])
@@ -19,15 +39,7 @@ def test_class_correctly_classified(inp_a, label, predictor, vectorizer, dim_red
     """Model predicts classes correctly"""
     label_a = get_label(text=inp_a, predictor=predictor, vectorizer=vectorizer, dim_reducer=dim_reducer)
     assert label_a[label.index(1)] == 1
-#
-#
-# @pytest.mark.parametrize("input, label", [("Hello world", 1), ("Hello earth", 1)])
-# def test_mft(input, label, predictor, vectorizer):
-#     """Minimum Functionality Tests (simple input/output pairs)"""
-#     prediction = get_label(text=input, predictor=predictor, vectorizer=vectorizer)
-#     assert label == prediction
-#
-#
+
 
 
 def get_label(text, predictor, vectorizer, dim_reducer):
