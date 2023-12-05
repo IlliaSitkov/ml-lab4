f0154d85fa77cdc864f7b17c5526a8842d532ac1
Illia Sitkov <illiasitkov@gmail.com>
2023-12-05 22:55:49 +0200

    add inference script and tests

---
+++ src/classifier.py
@@ -0,0 +1,47 @@
+import sys
+import requests
+from io import BytesIO
+import joblib
+from preproc import preprocess_comments
+
+
+def load_model_from_github(model_name):
+    github_url = f'https://github.com/illiasitkov/ml-lab4/raw/main/data/{model_name}.joblib'
+    response = requests.get(github_url)
+    if response.status_code == 200:
+        model = joblib.load(BytesIO(response.content))
+        return model
+    else:
+        print(f"Failed to fetch {model_name} from GitHub.")
+        sys.exit(1)
+
+
+def classify_text(text, predictor, vectorizer, dim_reducer):
+    text_preproc, = preprocess_comments([text])
+    vect = vectorizer.transform([text_preproc])
+    vect_reduced = dim_reducer.transform(vect.toarray())
+    res, = predictor.predict(vect_reduced)
+    return res.tolist()
+
+
+def main():
+    classifier = load_model_from_github('my_model')
+    vectorizer = load_model_from_github('vectorizer')
+    pca_model = load_model_from_github('pca_model')
+
+    if len(sys.argv) < 2:
+        print("Please provide the text to classify as a command-line argument")
+        sys.exit(1)
+
+    text_to_classify = sys.argv[1]
+
+    predictions = classify_text(text_to_classify, classifier, vectorizer, pca_model)
+
+    print("Predictions:")
+
+    for class_name, class_value in zip(['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate'], predictions):
+        print(f"{class_name}: {'False' if class_value == 0 else 'True'}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/inference/test_classifier.py
@@ -0,0 +1,18 @@
+import subprocess
+
+
+def test_classifier():
+    command = [
+        'python',
+        'classify_text.py',
+        'This is a test comment for classification.'
+    ]
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    assert result.returncode == 0, f"Script execution failed with error: \n{result.stderr}"
+    assert "toxic:" in result.stdout
+    assert "severe_toxic:" in result.stdout
+    assert "obscene:" in result.stdout
+    assert "threat:" in result.stdout
+    assert "insult:" in result.stdout
+    assert "identity_hate:" in result.stdout
+
